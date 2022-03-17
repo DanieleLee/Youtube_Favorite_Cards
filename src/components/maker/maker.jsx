bf8a04f6c8f66d1ca1acdaff347a98ea5fba036e
DanieleLee <keojin28@gmail.com>
2022-03-17 22:40:53 +0900

    'makeCardUI'

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Header from '../header/header';
@@ -11,7 +11,44 @@ const Maker = (props) => {
     //     service.logout();
     //     navigate(-1);
     // }
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'Daniele',
+            company: 'MicroSoft',
+            theme: 'dark',
+            title: 'Software Engineer',
+            email: 'daniele@gmail.com',
+            message: 'go together',
+            fileName: 'untitle1',
+            fileURL: null
 
+        },
+        {
+            id: '2',
+            name: 'Daniele2',
+            company: 'Google',
+            theme: 'colorful',
+            title: 'Software Engineer',
+            email: 'daniele2@gmail.com',
+            message: 'go together',
+            fileName: 'untitle1',
+            fileURL: ''
+
+        },
+        {
+            id: '3',
+            name: 'Daniele3',
+            company: 'Netmable',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'daniele3@gmail.com',
+            message: 'go together',
+            fileName: 'untitle1',
+            fileURL: ''
+
+        }
+    ]);
     useEffect(() => {
         props.authservice.onAuthChanged(user => {
             if(!user){
@@ -25,8 +62,8 @@ const Maker = (props) => {
         <section className={styles.maker}>
             <Header onlogout={props.onclick}></Header>
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards}/>
+                <Preview cards={cards}/>
 
             </div>
         </section>
