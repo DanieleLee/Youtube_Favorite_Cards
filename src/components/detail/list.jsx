d26e4c2d0f1daf6134a7e547bc340e6143e3ef1d
DanieleLee <keojin28@gmail.com>
2022-09-04 21:26:03 +0900

    'design_list2'

--- src/components/detail/list.jsx
+++ src/components/detail/list.jsx
@@ -10,24 +10,33 @@ const List = memo (
     if(item != undefined)
     {
       const img_src1 = item.snippet.thumbnails.default.url;
+      const item_id = item.id;
       const c_title_src1 = item.snippet.channelTitle;
       const title_src1 = item.snippet.title;    
     
       return(
           <>
-            <li className={styles.cards__item} onClick={onClick}>
-              <figure className={styles.cards__item__pic_wrap}>
-                <img  src={img_src1} className={styles.cards__item__img}></img>
-              </figure>
-              <div className={styles.cards__item__info} id='box2'>
-                <p className={styles.cards__item__text}>
-                {title_src1}
-                {/* <br />
-                [{c_title_src1}] */}
-                </p>
-                   
-              </div>
-            </li>
+          <div className={styles.cards__item__div}>
+            <a  href={'https://www.youtube.com/watch?v=' + item_id}>
+              <li className={styles.cards__item} onClick={onClick}>
+                <figure className={styles.cards__item__pic_wrap}>
+                  <img  src={img_src1} className={styles.cards__item__img}></img>
+                </figure>
+                <div className={styles.cards__item__info} id='box2'>
+                  <p className={styles.cards__item__text}>
+                  {title_src1}
+                  {/* <br />
+                  [{c_title_src1}] */}
+                  </p>
+                </div>
+              </li>
+              
+            </a>
+
+            <div className={styles.cards__item__hoverbox}>
+              <a href={'https://www.youtube.com/watch?v=' + item_id}></a>
+            </div>
+          </div>
           </>
         );
       }   
